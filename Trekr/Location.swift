c7304870e8a8e317abf8f4cc4979f6ef0abf15f9
tchamalsha <tharu.chamalsha@gmail.com>
2022-08-02 10:32:51 +0530

    add location file

--- Trekr/Location.swift
+++ Trekr/Location.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Location{
+    
+    let id: Int
+    let name: String
+    let country: String
+    let description: String
+    let more: String
+    let heroPicture: String
+    
+    static let example = Location (id:1,name:"The highlands",country:"United states",description:"Nice place to visit",
+                           more:"more text here.",heroPicture: "highlands"
+    )
+}
