b2d0c16d53f7cca09d21965ac7020c24476f3848
tchamalsha <tharu.chamalsha@gmail.com>
2022-08-02 08:54:24 +0530

    Initial Commit

---
+++ Trekr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trekr
+//
+//  Created by Tharushi Chamalsha on 2022-08-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Tharushi Chamalsha on 2022-08-02.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
